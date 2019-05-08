638e9f9b7c5f7353a5a4e8f937ccc4bbb8be7263
azinoVo <azinovo22@gmail.com>
2019-05-07 19:00:25 -0500

    NavLinks added with styles

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
   }
-
+// Styles only appear when the URL matches the movie id
   render() {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie"><Link to={`/movies/${movie.id}`}>{movie.title}</Link></span>
+          <span className="saved-movie">
+            <NavLink
+              to={`/movies/${movie.id}`}
+              activeStyle={{
+                color: "black",
+                border: "1px solid blue",
+                textDecoration: "none"
+              }}>
+                {movie.title}
+            </NavLink>
+          </span>
         ))}
         <Link to='/'><div className="home-button">Home</div></Link>
       </div>
